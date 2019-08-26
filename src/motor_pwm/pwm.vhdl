dceb2347d36b134137ceea1bde0f2fd3a87a448b
th3sh3ph3rd <jan.nausner@gmail.com>
2019-08-26 13:55:04 +0200

    clean fix of pwm overflow

--- src/motor_pwm/pwm.vhdl
+++ src/motor_pwm/pwm.vhdl
@@ -43,9 +43,8 @@ architecture behavior of pwm is
 
     type dc_cnt is array(0 to PWM_CHANNELS) of natural range 0 to PWM_CLKS-1;
     signal dc_mid, dc_mid_next : dc_cnt;
-    --type mul is array(0 to PWM_CHANNELS) of natural range 0 to (((2**PWM_DC_RES)-1)*124999); --account for multiplication overflow
-    --constant MAX_MUL_BITS : natural := natural(ceil(log2(real(((2**PWM_DC_RES)-1)*(PWM_CLKS-1))))); 
-    type mul is array(0 to PWM_CHANNELS-1) of unsigned(33 downto 0); --account for multiplication overflow
+    constant MAX_MUL_BITS : natural := natural(ceil(log2(real(2**PWM_DC_RES-1)*real(PWM_CLKS-1)))); 
+    type mul is array(0 to PWM_CHANNELS-1) of unsigned(MAX_MUL_BITS-1 downto 0); --account for multiplication overflow
     signal mul_tmp : mul;
 
 begin
@@ -77,13 +76,9 @@ begin
 
         if cnt = PWM_CLKS-1 then
             cnt_next <= 0;
-            for i in 0 to PWM_CHANNELS-1 loop
-                mul_tmp(i) <= dc_buf(i)*to_unsigned(PWM_CLKS, 18); --account for overflow
+            for i in 0 to PWM_CHANNELS-1 loop --calculate next pwm transition
+                mul_tmp(i) <= dc_buf(i)*to_unsigned(PWM_CLKS, MAX_MUL_BITS-PWM_DC_RES); --account for overflow
                 dc_mid_next(i) <= to_integer(mul_tmp(i)/(2**PWM_DC_RES));
---                mul_tmp(i) <= to_integer(dc_buf(i))*PWM_CLKS; --account for overflow
---                dc_mid_next(i) <= mul_tmp(i)/(2**PWM_DC_RES);
---                dc_mid_next(i) <= to_integer(to_unsigned(mul_tmp(i), 2**PWM_DC_RES-1));
---                dc_mid_next(i) <= to_integer(dc_buf(i))*PWM_CLKS/(2**PWM_DC_RES);
             end loop;
         else
             cnt_next <= cnt + 1;
